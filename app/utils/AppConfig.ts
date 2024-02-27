39d5d6eda03f8b0d1558e322b5b6f1f6d222e51e
greg-bix <gregorioescalona@gmail.com>
2024-02-27 16:44:49 -0400

    feat: rmove the README content

--- app/utils/AppConfig.ts
+++ app/utils/AppConfig.ts
@@ -1,11 +1,10 @@
-import type { LocalePrefix } from 'node_modules/next-intl/dist/types/src/shared/types';
+// import type { LocalePrefix } from 'node_modules/next-intl/dist/types/src/shared/types';
 
-const localePrefix: LocalePrefix = 'as-needed';
+// const localePrefix: LocalePrefix = 'as-needed';
 
 // FIXME: Update this configuration file based on your project information
 export const AppConfig = {
-  name: 'Arawato Media',
-  locales: ['en', 'fr'],
-  defaultLocale: 'en',
-  localePrefix,
-};
+  name: "Arawato Media",
+  locales: ["en", "fr"],
+  defaultLocale: "en",
+}
