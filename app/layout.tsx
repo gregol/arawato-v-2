59d0442ec5dbcf7d32f72dfc06394a4312269bf1
greg-bix <gregorioescalona@gmail.com>
2024-02-28 08:12:31 -0400

    fix: solving some load errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,13 +16,18 @@ export default function RootLayout ({ children }: { children: React.ReactNode })
             </header>
 
             <main>{children}</main>
-            <footer className="item-center md:flex-rows flex justify-center border-t border-gray-300 py-8">
-              <div className="item-center size-10 mr-6 justify-center rounded-full  bg-white">
-                <img src="assets/images/mono_header.png" width="40" alt="Arawato" />
+            <footer className="item-center md:flex-rows flex flex-row justify-center border-t border-gray-300 py-8">
+              <div className="item-center w-10 h-10 size-10 mr-6 justify-center rounded-full  bg-white">
+                <img src="assets/images/mono_header.png" alt="Arawato" />
               </div>
-              <div>
+              <div className="flex justify-center items-center">
                 <p className="px-1 text-sm text-white">
                   © Copyright {new Date().getFullYear()} {AppConfig.name}.
+                </p>
+              </div>
+              <div className="flex justify-center items-center">
+                <p className="px-1 text-sm text-white">
+                 Contact: admin@arawato.co +584265177411
                 </p>
               </div>
             </footer>
